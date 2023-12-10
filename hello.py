70628b45ed66fe78356f90cdf1e03f0c38129134
EC2 Default User <ec2-user@ip-172-31-26-138.us-west-1.compute.internal>
2023-12-10 06:14:38 +0000

    changes made to hello.py file for testing purposes

--- hello.py
+++ hello.py
@@ -1,19 +1,19 @@
 import click
-
 import boto3
-
-@click.command()
 
 def add(x,y):
     return x+y
-    
-    """This lists my aws s3 buckets"""
-    
+
+@click.command()
+def buckets():
+    """This lists my AWS S3 buckets"""
+
     s3 = boto3.client("s3")
-    all_buckets =s3.list_buckets()
+    all_buckets = s3.list_buckets()
     for bucket in all_buckets['Buckets']:
         click.echo(
-            click.style(f"bucket: {bucket['Name']}", bg='yellow', fg='blue'))
+            click.style(f"bucket: {bucket['Name']}", bg="yellow", fg="blue")
+        )
 
 
 if __name__ == "__main__":
